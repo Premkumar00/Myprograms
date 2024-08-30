deb20d92d77d0a629d9e636d92de7ad127720336
Premkumar00 <114053388+Premkumar00@users.noreply.github.com>
2024-08-30 12:22:40 +0530

    Update framework_sqlite.py

--- framework_sqlite.py
+++ framework_sqlite.py
@@ -9,7 +9,9 @@ data_table = cursor.execute(f"select value from {config_table} where key = ?", (
 data_table = data_table[0]
 
 field_names = cursor.execute(f"PRAGMA table_info({data_table})").fetchall()
-field_names	 = [data[1] for data in field_names]
+field_names	= [data[1] for data in field_names]
+
+message = cursor.execute(f"select value from {config_table} where key = ?", ("saved_msg",)).fetchone()
 
 def Create_record():
 	data = []
@@ -17,34 +19,45 @@ def Create_record():
 		data.append(input(f"Enter the {field}: "))
 	data = tuple(data)
 	cursor.execute(f"INSERT into {data_table} values {str(data)}")
+	print(f"Record created {message[0]}")
 
 def read_records():
 	data = cursor.execute(f"select * from {data_table}").fetchall()
 	if data:
 		for record in data:
-			print(record)
+			print(f"\n{field_names[0]} = {record[0]}\n{field_names[1]} = {record[1]}\n{field_names[2]} = {record[2]}")
 	else:
 		print("No data found")
 
 def update_record():
-	record_id = input(f"Enter {field_names[0]} to update: ")
+	record_id = input(f"\nEnter {field_names[0]} to update: ")
 	for index in range(1, len(field_names)):
 		print(f"{index}. Update {field_names[index]}")
 	choice = int(input("enter your choice to update: "))
-	cursor.execute(f"update {data_table} set {field_names[choice]} = ? where {field_names[0]} = ?",(input(f"Enter new {field_names[choice]}: "), record_id))
+	newtext = input(f"Enter new {field_names[choice]}: ")
+	cursor.execute(f"update {data_table} set \"{field_names[choice]}\" = \"{newtext}\" where \"{field_names[0]}\" = \"{record_id}\"")
+	print(f"Record {record_id} updated {message[0]}")
 
 def delete_record():
-	record_id = input(f"Enter {field_names[0]} to delete: ")
-	cursor.execute(f"delete from {data_table} where {field_names[0]} = ?", (record_id,))
+	record_id = input(f"\nEnter {field_names[0]} to delete: ")
+	cursor.execute(f"delete from {data_table} where \"{field_names[0]}\" = \"{record_id}\"")
+	print(f"Record with {record_id} deleted {message[0]}")
+
+def search_record():
+	record_id = input(f"\nEnter {field_names[0]} to search: ")
+	data = cursor.execute(f"select * from {data_table} where \"{field_names[0]}\" = \"{record_id}\"").fetchone()
+	if data:
+		print(f"\n{field_names[1]} = {data[1]}\n{field_names[2]} = {data[2]}")
+	else:
+		print(f"Record {record_id} is not found")
 
 def ShowMenu():
-	data = cursor.execute(f"SELECT * FROM {config_table} WHERE key = 'Menu'").fetchone()
-	data = eval(data[1])
-	for record in data:
-		print(record)
+	data = cursor.execute(f"SELECT value FROM {config_table} WHERE key = 'Menu'").fetchone()
+	data = data[0].replace("\\n", "\n")
+	print(data)
 
 while True:
 	ShowMenu()
-	Menu = [Create_record, read_records, update_record, delete_record, exit]
-	Menu[int(input("Enter your choice: ")) - 1]()
+	Menu = [Create_record, read_records, update_record, delete_record, search_record, exit]
+	Menu[int(input("\nEnter your choice: ")) - 1]()
 	conn.commit()
